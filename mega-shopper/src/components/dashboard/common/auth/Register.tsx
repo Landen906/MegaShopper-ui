b938ef9ffa1bce577ef35db45dd00e8d5fcbf8ae
Almustafa James <almustafa830@revature.net>
2022-06-17 14:58:40 -0500

    Register page in React

---
+++ mega-shopper/src/components/dashboard/common/auth/Register.tsx
@@ -0,0 +1,16 @@
+export const Login = () => {
+    return(
+<div>
+        First Name:
+        <input type="text" placeholder="First Name"/>
+         Last Name:
+          <input type="text" placeholder="Last Name" />
+        Email:
+        <input type="text" placeholder="Email" />
+        Address:
+        <input type="text" placeholder="Address" />
+        Password:
+        <input type="text" placeholder="Password" />
+        <input type="Submit" value="Submit" />
+    </div>
+)}
