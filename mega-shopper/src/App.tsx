d01a1f012ca09942893f31e73ed4e9f1490901ec
Landen906 <94669855+Landen906@users.noreply.github.com>
2022-06-18 16:14:34 -0400

    Added customer DataGrid function
    Added dependencies for data grid, changed EmployeeDashboard to CustomerList. Added CustomerList to APP.tsx(temp until proper placement is ready).

--- mega-shopper/src/App.tsx
+++ mega-shopper/src/App.tsx
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import lb4Provider from 'react-admin-lb4';
+import { Admin, Resource } from 'react-admin';
+import CustomerList from './components/dashboard/CustomerList';
 
 function App() {
   return (
+    <><Admin dataProvider={lb4Provider('http//localhost:5000')}>
+      <Resource name='customers' list={CustomerList} />
+    </Admin>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div></>
   );
 }
 
---
+++ mega-shopper/src/components/dashboard/CustomerList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {List, Datagrid, TextField, Filter, SearchInput} from 'react-admin';
+
+const CustomerFilter = (props) => (<Filter {...props}>
+    <SearchInput placeholder='Customer ID' source='customer_id'
+    resettable alwaysOn />
+</Filter>)
+
+function CustomerList(props){
+return(
+     <List {...props} filters={CustomerFilter}>
+        <Datagrid>
+            <TextField source='customer_id'/>
+            <TextField source='first_name'/>
+            <TextField source='last_name'/>
+            <TextField source='email'/>
+            <TextField source='address'/>
+            <TextField source='orders'/>
+        </Datagrid>
+
+     </List>
+    
+  );
+
+}
+
+export default CustomerList;
