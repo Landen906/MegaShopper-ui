44728bfd7f185c8fa4d18e6aeb52bd7cb480c161
Almustafa James <almustafa830@revature.net>
2022-06-20 17:24:12 -0500

    Register Page completed

--- mega-shopper/src/components/dashboard/common/auth/Register.tsx
+++ mega-shopper/src/components/dashboard/common/auth/Register.tsx
@@ -1,16 +1,52 @@
+import React, {SyntheticEvent, useState} from 'react'
+
 export const Login = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleFirst = () => {}
+    const handleLast = () => {}
+    const handleEmail = () => {}
+    const handleAddress = () => {}
+    const handlePassword = () => {}
+    const handleRegister = () => { 
+        fetch("/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        address: address,
+        password: password,
+        })
+    })
+
+    .then(res => res.json())
+    .then(user => {
+
+    }
+        ) 
+
+    }
     return(
 <div>
+
+        <h1>Sign Up</h1>
+
         First Name:
-        <input type="text" placeholder="First Name"/>
+        <input onChange={handleFirst} type="text" placeholder="First Name"/>
          Last Name:
-          <input type="text" placeholder="Last Name" />
+          <input onChange={handleLast} type="text" placeholder="Last Name" />
         Email:
-        <input type="text" placeholder="Email" />
+        <input onChange={handleEmail} type="text" placeholder="Email" />
         Address:
-        <input type="text" placeholder="Address" />
+        <input onChange={handleAddress} type="text" placeholder="Address" />
         Password:
-        <input type="text" placeholder="Password" />
-        <input type="Submit" value="Submit" />
+        <input onChange={handlePassword} type="text" placeholder="Password" />
+        <button onClick={handleRegister}> Sign Up </button>
     </div>
 )}
