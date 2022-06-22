96df8cca5fa90149e09fd2027030a29bb39ce3ad
Almustafa James <almustafa830@revature.net>
2022-06-22 14:26:55 -0500

    Register Page mock fetch

--- mega-shopper/src/components/dashboard/common/auth/Register.tsx
+++ mega-shopper/src/components/dashboard/common/auth/Register.tsx
@@ -1,11 +1,12 @@
 import React, {SyntheticEvent, useState} from 'react'
 
 export const Register = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [address, setAddress] = useState("");
-    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState<string>();
+    const [lastName, setLastName] = useState<string>();
+    const [email, setEmail] = useState<string>();
+    const [address, setAddress] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [errors, setError] = useState<string>();
 
     const handleFirst = () => {}
     const handleLast = () => {}
@@ -25,7 +26,11 @@ export const Register = () => {
         })
     })
 
-    .then(res => res.json())
+    .then(res => {
+        if (res.status != 200) {
+            setError("Must Provide Required Credentials To Sign Up");
+        }
+    })
     .then(user => {
 
     }
@@ -33,20 +38,19 @@ export const Register = () => {
 
     }
     return(
-<div>
-
-        <h1>Sign Up</h1>
-
-        First Name:
-        <input onChange={handleFirst} type="text" placeholder="First Name"/>
-         Last Name:
-          <input onChange={handleLast} type="text" placeholder="Last Name" />
-        Email:
-        <input onChange={handleEmail} type="text" placeholder="Email" />
-        Address:
-        <input onChange={handleAddress} type="text" placeholder="Address" />
-        Password:
-        <input onChange={handlePassword} type="text" placeholder="Password" />
-        <button onClick={handleRegister}> Sign Up </button>
-    </div>
-)}
+        <div>
+            <h1>Sign Up</h1>
+            First Name:
+            <input onChange={handleFirst} type="text" placeholder="First Name"/>
+            Last Name:
+            <input onChange={handleLast} type="text" placeholder="Last Name" />
+            Email:
+            <input onChange={handleEmail} type="text" placeholder="Email" />
+            Address:
+            <input onChange={handleAddress} type="text" placeholder="Address" />
+            Password:
+            <input onChange={handlePassword} type="text" placeholder="Password" />
+            <button onClick={handleRegister}> Sign Up </button>
+        </div>
+    )
+}
