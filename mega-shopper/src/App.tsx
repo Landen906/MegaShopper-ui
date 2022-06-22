7e79492989ea6baa51ab50739123f6be935acb29
Almustafa James <almustafa830@revature.net>
2022-06-22 16:25:42 -0500

    Added Error messages to Register Page, changed imports on App.tsx, added functionality to register functions, added employee dashboard route

--- mega-shopper/src/App.tsx
+++ mega-shopper/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserDashboard from './components/dashboard/UserDashboard';
 import { Login } from './components/dashboard/common/auth/Login';
 import { Register } from './components/dashboard/common/auth/Register';
+import EmployeeDashboard from './components/dashboard/EmployeeDashboard';
 
 function App() {
   return (
       <div className="App">
         <BrowserRouter>
           <Routes>
+            <Route path="/employeedashboard" element={<EmployeeDashboard currentUser={undefined}/>}/>
             <Route path="/register" element={<Register/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/dashboard" element={<UserDashboard/>} />
