e645359cbd1d08b7c09c8d01d24042237fb47576
Sirgourny Compton <sirgournycompton@Sirgournys-iMac.local>
2022-06-22 16:38:32 -0500

    Changed customerId from an 'int' to a 'string'

--- mega-shopper/src/components/dashboard/UserDashboard.tsx
+++ mega-shopper/src/components/dashboard/UserDashboard.tsx
@@ -1,3 +1,5 @@
+
+
 import { User } from "../../models/user";
 
 interface IDashboardProps {
@@ -8,7 +10,7 @@ function UserDashboard(props: IDashboardProps) {
 
 
     return (
-            !props.currentUser ? <p>You are not logged in!</p> :
+            !props.currentUser ? <Navigate to="/login"/> :
             <>
                 <h1>Welcome, {props.currentUser.firstName}!</h1>
             </>
@@ -17,3 +19,16 @@ function UserDashboard(props: IDashboardProps) {
 
 }
 export default UserDashboard;
+
+// TODO: - add Route path in App.tsx [under ""function App()""]
+/*    return (
+        <Routes>
+        <Route path="/dashboard" element={<Dashboard currentUser={authUser}/>}/>
+        </Routes>
+      );
+       */
+
+// TODO: - add add props Navigated to dashboard in Login.tsx [above: <h4>Log into Mega-Shopper!</4>]
+/*      return (
+            props.currentUser ? <Navigate to="/dashboard"/> :
+       */
