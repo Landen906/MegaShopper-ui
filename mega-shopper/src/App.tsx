0e970f72fd5facf58d0050c5aac3fd4f1d635428
Yasin <yasinabdussaburr@gmail.com>
2022-06-22 15:13:15 -0400

    removed unused imports from App.tsx

--- mega-shopper/src/App.tsx
+++ mega-shopper/src/App.tsx
@@ -1,9 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-// import lb4Provider from 'react-admin-lb4';
-import { Admin, Resource } from 'react-admin';
-// import CustomerList from './components/dashboard/CustomerList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserDashboard from './components/dashboard/UserDashboard';
 import { Login } from './components/dashboard/common/auth/Login';
