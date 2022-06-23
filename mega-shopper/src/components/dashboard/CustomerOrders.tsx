01a44307d1119de0d4906a01e40c7c4010d19da6
Richard Moch <ibmpcdos5@gmail.com>
2022-06-23 13:15:21 -0500

    add structure to Cart.tsx, set props type to any

--- mega-shopper/src/components/dashboard/CustomerOrders.tsx
+++ mega-shopper/src/components/dashboard/CustomerOrders.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {List, Datagrid, TextField, Filter, SearchInput} from 'react-admin';
 
-const OrderFilter = (props) => (<Filter {...props}>
+const OrderFilter = (props: any) => (<Filter {...props}>
     <SearchInput placeholder='Customer ID' source='customer_id'
     resettable alwaysOn />
 </Filter>)
 
-function CustomerOrders(props){
+function CustomerOrders(props: any){
 return(
      <List {...props} filters={OrderFilter}>
         <Datagrid>
