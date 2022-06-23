33931fab937462c0232bf0685c80cd72351da6c5
Richard Moch <ibmpcdos5@gmail.com>
2022-06-23 13:40:31 -0500

    add filter to cart, add routing to app.tsx

--- mega-shopper/src/components/dashboard/Cart.tsx
+++ mega-shopper/src/components/dashboard/Cart.tsx
@@ -1,1 +1,20 @@
 import { Navigate } from 'react-router-dom';
+import { User } from '../../models/user';
+
+import {List, Datagrid, TextField, Filter, SearchInput} from 'react-admin';
+
+const ItemFilterBy = (props: any) => (
+    <Filter {...props}>
+        <SearchInput placeholder='Search items...' source='title' resettable alwaysOn>
+        </SearchInput>
+    </Filter>   
+);  // would it make sense to use the item title? 
+
+function Cart(props: any) {
+
+    return(
+        <div><p>This is a test!</p></div>
+    );
+}
+
+export default Cart;
