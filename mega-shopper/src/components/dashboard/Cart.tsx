99033e294234bd55fb70dfecfbdaa6df9f7384e0
Richard Moch <ibmpcdos5@gmail.com>
2022-06-24 12:06:01 -0500

    fix barebones cart

--- mega-shopper/src/components/dashboard/Cart.tsx
+++ mega-shopper/src/components/dashboard/Cart.tsx
@@ -2,31 +2,34 @@ import { Navigate } from "react-router-dom";
 import { User } from "../../models/user";
 
 import { ItemType } from "../../models/item";
+import { ReactElement } from "react";
+import { AnyRecord } from "dns";
+import { createInterface } from "readline";
 const items: Array<ItemType> = [
-    {
-        id: 1,
-        title: "Testing",
-        desc: "Test description",
-        price: 1.99,
-        categoryId: 1,
-        img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
-    },
-    {
-        id: 2,
-        title: "Testing",
-        desc: "Test description",
-        price: 1.99,
-        categoryId: 1,
-        img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
-    },
-    {
-        id: 3,
-        title: "Testing",
-        desc: "Test description",
-        price: 1.99,
-        categoryId: 1,
-        img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
-    },
+	{
+		id: 1,
+		title: "Testing",
+		desc: "Test description",
+		price: 1.99,
+		categoryId: 1,
+		img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
+	},
+	{
+		id: 2,
+		title: "Testing",
+		desc: "Test description",
+		price: 1.99,
+		categoryId: 1,
+		img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
+	},
+	{
+		id: 3,
+		title: "Testing",
+		desc: "Test description",
+		price: 1.99,
+		categoryId: 1,
+		img: "https://media.discordapp.net/attachments/535920055121346563/989735250206797824/20220623_223411.jpg",
+	},
 ];
 // const ItemFilter = (props: any) => (
 // 	<Filter {...props}>
@@ -40,29 +43,23 @@ const items: Array<ItemType> = [
 // ); // would it make sense to use the item title?
 
 // TO-DO: add functions pulling from the api
-function Cart(): any {
+function Cart() {
 	//to-do: put this in the props
-	
-	items.forEach(item => {
-        return (
-            <>
-                <li className="collection-item avatar" key={item.id}>
-                    <div className="image">
-                        <img src={item.img}></img>
-                        </div>
-                    <div className="description">
-                        <p>{item.title}</p>
-                        <p>{item.desc}</p>
-                        <p>Price: {item.desc}</p>
-                    </div>
-                </li>
-		    </>
-        );       
-    });		
-    
+
+	const cartItem = items.map((item) => {
+		
+        return <li className="collection-item avatar" key={item.id}>
+			<div className="image">
+				<img src={item.img}></img>
+			</div>
+			<div className="description">
+				<p>{item.title}</p>
+				<p>{item.desc}</p>
+				<p>Price: {item.price}</p>
+			</div>
+		</li>
+	});
+    return (<div>{cartItem}</div>);     
 }
 
-function getItems(items: Array<ItemType>) {
-
-}
 export default Cart;
