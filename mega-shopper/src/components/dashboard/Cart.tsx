961b0e853c98bb934da291ca40d264c84b71e5b4
Richard Moch <ibmpcdos5@gmail.com>
2022-06-23 22:45:07 -0500

    add hardcoded array of items

--- mega-shopper/src/components/dashboard/Cart.tsx
+++ mega-shopper/src/components/dashboard/Cart.tsx
@@ -3,6 +3,8 @@ import { User } from '../../models/user';
 
 import {List, Datagrid, TextField, Filter, SearchInput} from 'react-admin';
 
+import { ItemType } from '../../models/item';
+
 const ItemFilter = (props: any) => (
     <Filter {...props}>
         <SearchInput placeholder='Search items...' source='title' resettable alwaysOn>
@@ -12,14 +14,10 @@ const ItemFilter = (props: any) => (
 
 
 // TO-DO: add functions pulling from the api
-function Cart(props: Array<any>) {
-    props.map(item => {
-        return (
-            <li className='collection-item avatar' key={item.id}>
-                
-            </li>
-        )
-    });
+function Cart(props: Array<any>) { //to-do: put this in the props
+    let items: Array<ItemType> = [{id: 1, title: 'Testing', desc: 'Test description', price: 1.99, categoryId: 1}, 
+    {id: 2, title: 'Testing', desc: 'Test description', price: 1.99, categoryId: 1},
+    {id: 3, title: 'Testing', desc: 'Test description', price: 1.99, categoryId: 1}];
 }
 
 export default Cart;
