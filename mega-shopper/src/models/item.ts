5ca137182c38331308e0d66fc9022788992e2759
Landen906 <94669855+Landen906@users.noreply.github.com>
2022-06-27 17:29:39 -0400

    deleted nested folder

--- mega-shopper/src/models/item.ts
+++
@@ -1,8 +0,0 @@
-export type ItemType = {
-    id: number,
-    title: string,
-    desc: string,
-    price: number, // typescript only has a number type, not dec. 
-    categoryId: number,
-    img: string
-}
